2dea36343b4e67c409ede3ddf8a74b67be6ea9ba
Juan Sebastian <sebastianobh@gmail.com>
2025-04-28 10:23:19 -0500

    Sync

--- becerrahernandez_quiz3/backend/routes/router.js
+++ becerrahernandez_quiz3/backend/routes/router.js
@@ -3,14 +3,7 @@ const router = Router();
 import { login, saveLogin, validateToken, validate } from '../controllers/authController.js';
 import { getItems  } from '../controllers/itemsController.js';
 
-router.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    if (email === 'test@example.com' && password === '123456') {
-        res.status(200).json({ token: 'fake-jwt-token' });
-    } else {
-        res.status(401).json({ message: 'Usuario o contraseña inválidos' });
-    }
-});
+router.post('/login', login):
 router.post('/validateToken', validateToken, validate);
 router.post('/loginWithBiometric', validateToken, login);
 router.post('/saveBiometric', validateToken, saveLogin);
